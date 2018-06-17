65159783c7b69624dffc6f3e4510072aa03a99b9
tbalden <illespal@gmail.com>
2018-06-17 03:12:43 -0700

    dynamic eas boost: tune up level 2 to support gaming perf need

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -54,27 +54,29 @@ module_param(dynamic_stune_boost, uint, 0644);
 static bool shown_debug_stune = false;
 static bool shown_debug_input = false;
 static void uci_user_listener(void) {
-	boost_eas = uci_get_user_property_int_mm("boost_eas", 0, 0, 1);
+	boost_eas = uci_get_user_property_int_mm("boost_eas", 1, 0, 1);
 	boost_eas_level = uci_get_user_property_int_mm("boost_eas_level", 1, 0, 2);
 	boost_eas_level_ext = uci_get_user_property_int_mm("boost_eas_level_ext", 0, 0, 1);
 	pr_info("%s [CLEANSLATE] stune uci user listener %d %d %d\n",__func__,boost_eas,boost_eas_level,boost_eas_level_ext);
 	shown_debug_stune = false;
 	shown_debug_input = false;
 }
+static int boost_map[3] = { 9, 13, 20 };
 static int get_dynamic_stune_boost(void) {
 	int ret = 0;
 	if (boost_eas_level_ext) return dynamic_stune_boost;
-	ret = 9 + boost_eas_level * 3; // 9 - 15;
+	ret = boost_map[boost_eas_level]; // 9 - 20;
 	if (!shown_debug_stune) {
 		shown_debug_stune = true;
 		pr_info("%s [CLEANSLATE] dynamic stune boost value %d\n",__func__,ret);
 	}
 	return ret;
 }
+static int input_ms_map[3] = { 1300, 1500, 1500 };
 static int get_input_boost_ms(void) {
 	int ret = 0;
 	if (!boost_eas || boost_eas_level_ext) return input_boost_ms;
-	ret = 1000 + boost_eas_level * 300; // 1000 - 1600 msec;
+	ret = input_ms_map[boost_eas_level]; // 1300 - 1500 msec;
 	if (!shown_debug_input) {
 		shown_debug_input = true;
 		pr_info("%s [CLEANSLATE] input boost ms value %d\n",__func__,ret);
