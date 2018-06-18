05293cbd070894f55ccf7ab4f7ec16937d6fc2b9
joshuous <joshuous@gmail.com>
2018-06-18 15:47:01 +0200

    cpu-boost: Reset Dynamic SchedTune Boost only if it is currently active
    We need to ensure that every do_stune_boost() that is called is met by
    the same number of reset_stune_boost() so that the stune_boost_count is
    managed correctly.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -50,6 +50,7 @@ module_param(input_boost_ms, uint, 0644);
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
+static bool stune_boost_active;
 #ifdef CONFIG_UCI
 static bool shown_debug_stune = false;
 static bool shown_debug_input = false;
@@ -225,7 +226,10 @@ static void do_input_boost_rem(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	reset_stune_boost("top-app");
+	if (stune_boost_active) {
+		reset_stune_boost("top-app");
+		stune_boost_active = false;
+	}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	/* Update policies for all online CPUs */
@@ -248,6 +252,11 @@ static void do_input_boost(struct work_struct *work)
 	if (sched_boost_active) {
 		sched_set_boost(0);
 		sched_boost_active = false;
+	}
+
+	if (stune_boost_active) {
+		reset_stune_boost("top-app");
+		stune_boost_active = false;
 	}
 
 	/* Set the input_boost_min for all CPUs in the system */
@@ -273,10 +282,14 @@ static void do_input_boost(struct work_struct *work)
 	/* Set dynamic stune boost value */
 #ifdef CONFIG_UCI
 	if (boost_eas || boost_eas_level_ext) {
-		do_stune_boost("top-app", get_dynamic_stune_boost());
+		ret = do_stune_boost("top-app", get_dynamic_stune_boost());
+		if (!ret)
+			stune_boost_active = true;
 	}
 #else
-	do_stune_boost("top-app", dynamic_stune_boost);
+	ret = do_stune_boost("top-app", dynamic_stune_boost);
+	if (!ret)
+		stune_boost_active = true;
 #endif
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
