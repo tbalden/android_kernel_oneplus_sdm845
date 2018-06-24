e298a4b1e4dac6f7221ea8d16d529488a6e8ef93
tbalden <illespal@gmail.com>
2018-06-24 16:31:49 -0700

    uci: ntf: add notification flashlight/vib notif
    phase #1
    
    @tbalden

--- include/linux/notification/notification.h
+++ include/linux/notification/notification.h
@@ -17,6 +17,13 @@ enum notif_smart_level_type {
         NOTIF_STOP // stop overall
 };
 
+#define NTF_EVENT_NOTIFICATION "notification"
+#define NTF_EVENT_RINGING "ringing"
+#define NTF_EVENT_CHARGE_STATE "charge_state"
+#define NTF_EVENT_CHARGE_LEVEL "charge_level"
+#define NTF_WAKE_BY_USER "wake_by_user"
+
+
 extern void smart_set_last_user_activity_time(void);
 extern int smart_get_notification_level(int notif_type);
 
@@ -28,6 +35,14 @@ extern bool ntf_is_screen_early_off(void);
 // charge callbacks to notify ntf - call it from battery/policy drivers
 extern void ntf_set_charge_state(bool on);
 extern void ntf_set_charge_level(int level);
+
+// flashlight
+extern void ntf_set_cam_flashlight(bool on);
+
+// was the screen wake by user input...
+extern bool ntf_wake_by_user(void);
+// signal a user input (use it in touchscreen, input drivers..)
+extern void ntf_input_event(const char* caller, const char *param);
 
 /** add change listener */
 extern void ntf_add_listener(void (*f)(char* event, int num_param, char* str_param));
